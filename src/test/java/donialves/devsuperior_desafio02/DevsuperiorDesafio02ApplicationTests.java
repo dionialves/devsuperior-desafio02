0d4c5582fac83c5dae137a6383558cb1ff929c0e
Dioni Alves <dionialves@gmail.com>
2025-03-07 16:04:02 -0300

    Initial commit with bacic project files

---
+++ src/test/java/donialves/devsuperior_desafio02/DevsuperiorDesafio02ApplicationTests.java
@@ -0,0 +1,13 @@
+package donialves.devsuperior_desafio02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevsuperiorDesafio02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
